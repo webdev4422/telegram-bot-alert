16534470bedcd04204190c7d28c8d5a683934c1f
user <user@MacBook-Pro-user.local>
2024-01-06 16:40:23 +0200

    Add setInterval

--- index.js
+++ index.js
@@ -8,25 +8,19 @@ dotenv.config()
 async function run() {
   // Launch the browser and open a new blank page
   const browser = await puppeteer.launch({ headless: 'new' })
-  // const browser = await puppeteer.launch({ headless: false })
   const page = await browser.newPage()
 
   // Navigate the page to a URL
   await page.goto('https://alerts.in.ua')
-  // await page.goto('https://t.me/s/alarmukraine')
-  // await page.goto('https://x-drive.ua')
   // await new Promise((r) => setTimeout(r, 2000))
-  // document.querySelector('#__layout > div > main > h2').innerHTML
-  // await page.screenshot({ path: 'example.png' })
-  // const html = await page.content();
 
-  // Proceed
+  // Wait for element to be loaded
   await page.waitForSelector('#super-lite-map > g.oblasts > path:nth-child(22)')
 
   const data = await page.evaluate(() => {
-    // Get element in browser context
-    // Info: after many tries, I found that svg 'path' is not html element, but it is ATTRIBUTE, thiw why, when logging html elements or nodes of html partent element of 'path' it show nothing -> 'HTMLUnknownElement'. Thus access data attributes with 'attributes' method
-    // document.querySelector('#super-lite-map > g.oblasts > path:nth-child(22)').innerHTML // return empty, there are not html, only attributes
+    // Browser context
+    // Info: after many tries, I found that svg 'path' is not general html element, but it is related to ATTRIBUTE, this why, when logging html elements or nodes of html partent element of 'path' it show nothing -> 'HTMLUnknownElement'. Thus access data attributes with 'attributes' method
+    // document.querySelector('#super-lite-map > g.oblasts > path:nth-child(22)').innerHTML // return empty, there are no html, only attributes
     const svgPath = document.querySelector('#super-lite-map > g.oblasts > path:nth-child(22)')
     // let oblast = svgPath.attributes['data-oblast'].value
     let alertId = null
@@ -39,14 +33,18 @@ async function run() {
   const telegramChatId = process.env.TELEGRAM_CHAT_ID
   const telegramMessage = 'Повітряна тривога у Закарпатській області'
 
-  if (data) {
-    const response = await fetch(
-      `https://api.telegram.org/${telegramBotToken}/sendMessage?chat_id=${telegramChatId}&text=${telegramMessage}`
-    )
-    // const resData = await response.json()
-    // console.log(resData)
-  }
+  // if (data) {
+  const response = await fetch(
+    `https://api.telegram.org/${telegramBotToken}/sendMessage?chat_id=${telegramChatId}&text=${telegramMessage}`
+  )
+  // const resData = await response.json()
+  // console.log(resData)
+  // }
 
   await browser.close()
 }
-run()
+
+// Run every 20 sec
+setInterval(() => {
+  run()
+}, 20000)
